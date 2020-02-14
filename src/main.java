717ee1049069b54ad0f1b1fd23967bd9e1d15ab1
Mark Anderson <mande984@gmail.com>
2020-02-13 22:45:37 -0500

    Started implementation of matrix

---
+++ src/main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javafx.util.Pair;
+
+public class main {
+
+    /**
+     * Option for inputting the matrix or matrices through file input. File
+     * must be in format:
+     *  Number of Arrays
+     *  Array 1 Size
+     *  Array 2 Size (if none, size = 0)
+     *  Array 1, separated by spaces
+     *  Array 2, seperated by spaces
+     * @param fname the name of the input file
+     * @return the created 2d arrays from the file
+     */
+    private Pair<int[][], int[][]> readInputFile(String fname){
+        //TODO: Insert functionality to read through a file in correct format
+        try{
+            BufferedReader read = new BufferedReader(new FileReader(fname));
+            String line = read.readLine();
+            while (line != null){
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
